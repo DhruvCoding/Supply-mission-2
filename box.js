2e0eb3235930a25cb22b0c828333d4502791529b
DhruvCoding <66657906+DhruvCoding@users.noreply.github.com>
2020-08-03 16:08:48 -0700

    Add files via upload

---
+++ box.js
@@ -0,0 +1,29 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          istatic: true,
+          'friction':1.2,
+          'density':2.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("red");
+      stroke("black");
+      strokeWeight(3);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
